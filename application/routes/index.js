0a66edd8632b6b33205c48a1a4f63649d6339abb
Bryan Yan <byan4@sfsu.edu>
2023-04-23 19:55:31 -0700

    partitioned my html files and relinked my videos

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Bryan Yan" });
 });
 
+router.get("/login", function(req,res){
+  res.render("login");
+})
+
+router.get("/registration", function(req,res){
+  res.render("registration");
+})
+
+router.get("/postvideo", function(req,res){
+  res.render("postvideo");
+})
+
+router.get("/profile", function(req,res){
+  res.render("profile");
+})
+
+router.get("/viewpost/:id(\\d+)", function(req,res){
+  res.render("viewpost");
+})
+
 module.exports = router;
